3d478dc86dde54f7d2b317f5065821ffdbb2dc05
endang-ismaya <endang.ismaya@gmail.com>
2025-02-03 09:37:33 +0700

    add none type

---
+++ py05_none.py
@@ -0,0 +1,21 @@
+print(None)
+print(type(None))
+
+
+# age = None
+
+
+# if age is None:
+#     print("user belum memasukkan umur-nya")
+# else:
+#     print("User sudah memasukkan umur-nya")
+
+
+# fungsi ini seharusnya mengembalikan nilai umur (20)
+# def get_my_age():
+#     print("My age is 20")
+    
+
+# age = get_my_age()
+# if age > 20:
+#     print("Your age is more than 20")
